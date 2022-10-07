083912d4f52b5299dcf083acd5c073043840db18
NataliaOrlova <natakarelskaya@yandex.ru>
2022-10-07 23:16:43 +0400

    task5??

--- task5.py
+++ task5.py
@@ -13,3 +13,14 @@
 # "poly_2.txt"
 # out
 # The contents of the files do not match!
+
+# ("poly.txt", 'r') as data1, open("poly_2.txt", 'r') as data2,
+
+data1 = open('poly.txt').read().split('\n')
+data2 = open('poly_2.txt').read().split('\n')
+if len(data1) != len(data2):
+        print("Количество строк не совпадает")   
+else: 
+    with open ("poly.txt", 'r') as data1, open("poly_2.txt", 'r') as data2, open ("sum_poly.txt", "w+") as result:
+        for line1, line2 in zip(data1, data2): 
+            result.write(line1.replace('= 0', '+ ').rstrip('\n') + line2)
