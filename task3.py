af23123b7f691035a2064729fad0846fe023665f
NataliaOrlova <natakarelskaya@yandex.ru>
2022-10-07 16:52:45 +0400

    task3

--- task3.py
+++ task3.py
@@ -17,3 +17,26 @@
 # out
 # [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
 # [6, 2, 3, 0, 9]
+
+from random import randint
+def create_random_list(count):
+    if count <= 0:
+        print("Введено некорректное значение, повторите попытку")
+        return []
+    ls =[]
+    for i in range(count):
+        ls.append(randint(0,count))
+    return(ls)
+
+def find_unique_num(ls1):
+    res_list = []
+    for i in range(len(ls1)):
+        if ls1.count(ls1[i]) == 1:
+            res_list.append(ls1[i])
+    return(res_list)
+
+
+num = int(input("Введите количество элементов: "))
+lst = create_random_list(num)
+print(lst)
+print(find_unique_num(lst))
