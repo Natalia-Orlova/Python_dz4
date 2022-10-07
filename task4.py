bdddb667c22a10a7a73bd406f70c80f2c14e8398
NataliaOrlova <natakarelskaya@yandex.ru>
2022-10-07 18:19:03 +0400

    task4

--- task4.py
+++ task4.py
@@ -19,3 +19,24 @@
 # 4*x^5 + 1*x^4 - 3*x^3 - 3 = 0
 # 4*x^2 - 4 = 0
 # 2*x^4 - 3*x^3 + 3*x^2 + 1*x^1 - 2 = 0
+from random import randint, choice
+k1 = int(input("Задайте натуральную степень: "))
+
+def polynomial(k):
+    if k <= 0:
+        print("Введено некорректное значение")
+    else:
+        with open("dz4.txt", "a", encoding="utf-8") as dz4:
+            y = ""
+            while k > 0:
+                idx = randint(0,4)
+                sign = ['+', '-']
+                s = choice(sign)
+                if idx == 0:
+                    k -= 1
+                else:
+                    y += (f"{idx}*x^{k} {s} ")
+                    k -= 1
+            dz4.write(f"{y}{idx+1} = 0\n")
+                    
+polynomial(k1)
