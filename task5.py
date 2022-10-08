7dcac670ac1cb3f63969329b9039bd59fa5dd4b6
Natalia Orlova <109914840+Natalia-Orlova@users.noreply.github.com>
2022-10-08 23:20:13 +0400

    Update task5.py

--- task5.py
+++ task5.py
@@ -24,3 +24,22 @@ else:
     with open ("poly.txt", 'r') as data1, open("poly_2.txt", 'r') as data2, open ("sum_poly.txt", "w+") as result:
         for line1, line2 in zip(data1, data2): 
             result.write(line1.replace('= 0', '+ ').rstrip('\n') + line2)
+
+        
+# from random import choice
+# def poly_sum(name_1: str, name_2: str):
+# with open(name_1, "r", encoding="utf-8") as my_f_1, \
+# open(name_2, "r", encoding="utf-8") as my_f_2:
+# first = my_f_1.readlines()
+# second = my_f_2.readlines()
+
+# if len(first) == len(second):
+# with open("sum_poly.txt", "a", encoding="utf-8") as my_f_3:
+# for i, v in enumerate(first):
+# my_f_3.write(f"{v[:-5]} + {second[i]}")
+# else:
+# print("The contents of the files do not match!")
+
+
+# # poly_sum(input("Enter the file name 'text_1.txt': "), input("Enter the file name 'text_2.txt': "))
+# poly_sum("poly.txt", "poly_2.txt")
